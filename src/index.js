6121d3a57b301b8e6288e64de34810c284b85b7a
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-08-24 20:52:21 -0300

    redux dev tools support

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import reducers from './reducers'
 import App from './components/App'
 
-const store = createStore(reducers)
-const rootElement = document.getElementById('root')
+const composeEnhancers = (
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose
+)
+
+const middleware = applyMiddleware()
+const enhancers = composeEnhancers(middleware)
+const store = createStore(reducers, enhancers)
 
 const application = (
   <Provider store={store}>
     <App />
   </Provider>
 )
+
+const rootElement = document.getElementById('root')
 
 render(application, rootElement)
