534ccfbb4975ba114eb756bf4d0ba709c7bd6ba4
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-08-24 13:52:26 -0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import reducers from './reducers'
 import App from './components/App'
 
+const store = createStore(reducers)
 const rootElement = document.getElementById('root')
-render(<App />, rootElement)
+
+const application = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+render(application, rootElement)
