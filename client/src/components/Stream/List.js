e6f1f961ff6d4dcbdd1c10061b5c9317ff7989a5
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-08-28 11:49:24 -0300

    add streams list page

--- client/src/components/Stream/List.js
+++ client/src/components/Stream/List.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { List, Button, Container } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { fetchStreams } from '../../actions'
 
-const List = () => {
-  return (
-    <div>
-      List
-    </div>
-  )
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams()
+  }
+
+  renderStreamButtons = (stream) => {
+    if (stream.userId === this.props.userId) {
+      return (
+        <List.Content floated="right">
+          <Button primary>Edit</Button>
+          <Button negative>Delete</Button>
+        </List.Content>
+      )
+    }
+  }
+
+  renderCreateButton = () => {
+    if (this.props.signedIn) {
+      return (
+        <Container textAlign="right">
+          <Button primary as={Link} to="/stream/new">Create Stream</Button>
+        </Container>
+      )
+    }
+  }
+
+  renderStreamItem = (stream) => {
+    return (
+      <List.Item key={stream.id}>
+        { this.renderStreamButtons(stream) }
+        <List.Icon verticalAlign="middle" name="camera" size="large" />
+        <List.Content>
+          <List.Header>{ stream.title }</List.Header>
+          <List.Description>{ stream.description }</List.Description>
+        </List.Content>
+      </List.Item>
+    )
+  }
+
+  render() {
+    return (
+      <Container>
+        <h2>Streams</h2>
+        <List celled>
+          { this.props.streams.map(this.renderStreamItem) }
+        </List>
+        { this.renderCreateButton() }
+      </Container>
+    )
+  }
 }
 
-export default List
+const mapStateToProps = ({ streams, auth }) => ({
+  streams: Object.values(streams),
+  userId: auth.userId,
+  signedIn: auth.isSignedIn,
+})
+
+const mapDispatchToProps = { fetchStreams }
+const addReduxProps = connect(mapStateToProps, mapDispatchToProps)
+
+export default addReduxProps(StreamList)
