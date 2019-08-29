0c5597d3613d33e2e9b0a95ccf5499e315bab857
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-08-29 11:39:36 -0300

    load stream on stream show page

--- client/src/components/Stream/Show.js
+++ client/src/components/Stream/Show.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import { connect } from 'react-redux'
+import { Container } from 'semantic-ui-react'
+import flv from 'flv.js'
 import { fetchStream } from '../../actions'
 
 class Show extends Component {
+  constructor (props) {
+    super(props)
+    this.videoRef = createRef()
+    this.player = null
+  }
+
   componentDidMount () {
     const { fetchStream, match } = this.props
     const streamId = match.params.id
     fetchStream(streamId)
+    this.createStreamPlayer()
+  }
+
+  componentDidUpdate () {
+    this.createStreamPlayer()
+  }
+
+  componentWillUnmount () {
+    this.player.destroy()
+  }
+
+  createStreamPlayer = () => {
+    const { match, stream } = this.props
+    const streamId = match.params.id
+
+    const playerAlreadyCreated = !!(this.player)
+    const streamNotLoaded = !(stream)
+    const noVideoRef = !(this.videoRef && this.videoRef.current)
+
+    if (playerAlreadyCreated || streamNotLoaded || noVideoRef) return
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${streamId}.flv`,
+    })
+
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
 
   render () {
@@ -14,10 +50,11 @@ class Show extends Component {
     if (!stream) return <div>Loading...</div>
 
     return (
-      <div>
+      <Container>
+        <video ref={this.videoRef} style={{ width: '100%'}} controls />
         <h1>{ stream.title }</h1>
         <h5>{ stream.description }</h5>
-      </div>
+      </Container>
     )
   }
 }
